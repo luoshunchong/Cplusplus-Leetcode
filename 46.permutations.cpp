ae4710dd0a992ef90fc5a0fb0489841ae1170734
luoshunchong <1774594236@qq.com>
2021-10-23 14:05:16 +0800

    2021.10.23

--- 46.permutations.cpp
+++ 46.permutations.cpp
@@ -16,24 +16,49 @@ using namespace std;
 class Solution
 {
 public:
+    // vector<vector<int>> permute(vector<int> &nums)
+    // {
+    //     vector<vector<int>> ans;
+    //     backtracking(nums, 0, ans);
+    //     return ans;
+    // }
+    // void backtracking(vector<int> &nums, int level, vector<vector<int>> &ans)
+    // {
+    //     if (level == nums.size() - 1)
+    //     {
+    //         ans.push_back(nums);
+    //         return;
+    //     }
+    //     for (int i = level; i < nums.size(); ++i)
+    //     {
+    //         swap(nums[i], nums[level]);
+    //         backtracking(nums, level + 1, ans);
+    //         swap(nums[i], nums[level]);
+    //     }
+    // }
+
+    vector<vector<int>> res;
+    vector<int> ve;
     vector<vector<int>> permute(vector<int> &nums)
     {
-        vector<vector<int>> ans;
-        backtracking(nums, 0, ans);
-        return ans;
+        vector<bool> used(nums.size(), false);
+        backtracking(nums, used);
+        return res;
     }
-    void backtracking(vector<int> &nums, int level, vector<vector<int>> &ans)
+    void backtracking(vector<int> &nums, vector<bool>& used)
     {
-        if (level == nums.size() - 1)
-        {
-            ans.push_back(nums);
+        if(ve.size()==nums.size()){
+            res.push_back(ve);
             return;
         }
-        for (int i = level; i < nums.size(); ++i)
-        {
-            swap(nums[i], nums[level]);
-            backtracking(nums, level + 1, ans);
-            swap(nums[i], nums[level]);
+        for(int i=0;i<nums.size();++i){
+            if (used[i] == true)
+                continue; // path里已经收录的元素，直接跳过
+            used[i] = true;
+            ve.push_back(nums[i]);
+            backtracking(nums, used);
+            used[i] = false;
+            ve.pop_back();
         }
     }
 };
--- 491.increasing-subsequences.cpp
+++ 491.increasing-subsequences.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem491.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode id=491 lang=cpp
  *
@@ -10,20 +18,22 @@ public:
     vector<vector<int>> res;
     vector<int> ve;
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        backtracking(nums,0);
+        backtracking(nums, 0);
         return res;
     }
-    void backtracking(vector<int>& nums,int pos){
+    void backtracking(vector<int>& nums, int pos){
         if(ve.size()>=2){
             res.push_back(ve);
-            return;
         }
+        unordered_set<int> uset;
         for(int i=pos;i<nums.size();++i){
-            if(i>0&&nums[i]<nums[i-1]){
+            if ((!ve.empty() && nums[i] < ve.back()) || uset.find(nums[i])!=uset.end())
+            {
                 continue;
             }
+            uset.insert(nums[i]);
             ve.push_back(nums[i]);
-            backtracking(nums,i+1);
+            backtracking(nums, i+1);
             ve.pop_back();
         }
     }
