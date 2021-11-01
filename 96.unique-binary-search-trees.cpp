a9f2eb13ad3033986f4873cd0102d5d7d13decaf
luoshunchong <1774594236@qq.com>
2021-11-01 11:23:12 +0800

    2021.11.1

--- 96.unique-binary-search-trees.cpp
+++ 96.unique-binary-search-trees.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem96.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode id=96 lang=cpp
  *
@@ -5,10 +13,24 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int numTrees(int n) {
-        
+/*
+这道题的难点在于如何推理出动态规划的递推公式。
+*/
+    int numTrees(int n)
+    {
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= i; ++j)
+            {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
     }
 };
 // @lc code=end
