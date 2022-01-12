596a6c8596c563f12f60124881200bba05753665
luoshunchong <1774594236@qq.com>
2022-01-12 23:05:59 +0800

    2022.1.12

---
+++ 141.linked-list-cycle.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=141 lang=cpp
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    //使用快慢指针
+    //快指针一次走2，慢指针一次走1，当快慢指针相遇的时候，则表示有环
+    bool hasCycle(ListNode *head) {
+        if (!head) return false;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
+
