cf9d1bfcf9b3e141aa33eb9c94cf9f1ee8e42762
luoshunchong <1774594236@qq.com>
2022-03-26 20:03:58 +0800

    2022.3.26

---
+++ cppTest/cin_cout.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a;
+    string b;
+    cin >> a;
+    getline(cin, b);
+
+    cout << a << endl;
+    cout << b << endl;
+    system("pause");
+    return 0;
+}
