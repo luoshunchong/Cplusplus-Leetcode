244ab0165ff23a41b05a204b7a91d8ea94a1d0f9
luoshunchong <1774594236@qq.com>
2022-07-01 00:03:34 +0800

    2022.6.30

--- cppTest/cin_cout.cpp
+++ cppTest/cin_cout.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    string a;
-    string b;
-    cin >> a;
-    getline(cin, b);
+// int main() {
+//     string a;
+//     string b;
+//     cin >> a;
+//     getline(cin, b);
 
-    cout << a << endl;
-    cout << b << endl;
-    system("pause");
-    return 0;
+//     cout << a << endl;
+//     cout << b << endl;
+
+//     system("pause");
+//     return 0;
+// }
+
+// [[1,2],[3,4],[5,6]]
+int main() {
+    vector<pair<int, int>> nums;
+    char temp;
+    int h, w;
+    while (cin >> temp) {
+        if(temp == ']') break;
+        cin >> temp >> h >> temp >> w >> temp;
+        nums.push_back({h, w});
+    }
+    cout << 1 << endl;
 }
