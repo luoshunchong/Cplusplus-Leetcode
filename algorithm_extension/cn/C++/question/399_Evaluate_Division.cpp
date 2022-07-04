e3cb933cafbbdfacdecbcf9b12589dd62d5ea4d8
luoshunchong <1774594236@qq.com>
2022-07-04 14:46:08 +0800

    2022.7.4

---
+++ algorithm_extension/cn/C++/question/399_Evaluate_Division.cpp
@@ -0,0 +1,18 @@
+// @algorithm @lc id=399 lang=cpp 
+// @title evaluate-division
+
+#include <stack>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "algm/algm.h"
+using namespace std;
+// @test([["a","b"],["b","c"]],[2.0,3.0],[["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]])=[6.00000,0.50000,-1.00000,1.00000,-1.00000]
+// @test([["a","b"],["b","c"],["bc","cd"]],[1.5,2.5,5.0],[["a","c"],["c","b"],["bc","cd"],["cd","bc"]])=[3.75000,0.40000,5.00000,0.20000]
+// @test([["a","b"]],[0.5],[["a","b"],["b","a"],["a","c"],["x","y"]])=[0.50000,2.00000,-1.00000,-1.00000]
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+
+    }
+};
